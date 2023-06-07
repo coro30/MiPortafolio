313c205fe328c4d87de5f6cbf27855eeb9399d5e
Mirna Parra <mirnaparra14@gmail.com>
2023-06-07 14:36:01 -0400

    Añadidas rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'contact/index'
-  get 'projects/index'
-  get 'home/index'
+  get '/contact', to: 'contact#index'
+  get '/projects', to: 'projects#index'
+  get '/home', to: 'home#index'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root 'home#index'
   # root "articles#index"
 end
